0bc8336062c45d719c6c28f858385f04bd3ce90f
VInicius <viniciuspuente_2@hotmail.com>
2017-02-08 12:52:07 -0200

    Issues fixed.

--- libs/boot.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var app = require('../libs/middlewares')();
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express Server escutando na porta ' +
-        app.get('port'));
-});
