fdff1b6c5e55d376c7b824a66dbbfbcf644a9365
VInicius <viniciuspuente_2@hotmail.com>
2017-02-12 12:05:59 -0200

    Port changed

--- config/express.js
+++ config/express.js
@@ -5,11 +5,12 @@ var bodyParser = require('body-parser');
 module.exports = function() {
   var app = express();
 
-  app.set(process.env.PORT || 5000);
+  app.set('port', 3000);
 
   app.use(express.static('./public'));
   app.set('view engine', 'ejs');
   app.set('views','./app/views');
+
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(require('method-override')());
