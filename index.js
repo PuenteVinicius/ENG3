9dce8198e99d3df57ab6c5c85bd2afd443e33d36
VInicius <viniciuspuente_2@hotmail.com>
2017-02-12 00:29:28 -0200

    Remote mongo added.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/jcomerce')
+require('./config/database.js')('mongodb://teste:teste@ds019886.mlab.com:19886/teste')
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express Server escutando na porta ' +
